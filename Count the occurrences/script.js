732396b57e4372902fdcde044a6ee3e73c20b52c
Jayanta Chatterjee <93910315+Jayanta1622@users.noreply.github.com>
2023-03-18 21:00:39 +0530

    Add files via upload

---
+++ Count the occurrences/script.js
@@ -0,0 +1,5 @@
+let input = "d s f f wf w wf w w zc g wf g d g rfd vgh t ddg t e ft f t etf r e r";
+
+let output = input.map((word,i) => {
+    
+})
---
+++ Only unique items are allowed/script.js
@@ -0,0 +1,3 @@
+let input = [1,3,4,2,3,2,4,5,6];
+let output = new Set(input);
+console.log(output);
